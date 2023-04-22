77d6e3651ba9160890fba80a9733f520a7cfc33a
Joshua Nussbaumer <jnussb3@students.towson.edu>
2023-04-21 21:26:41 -0400

    some function ideas I have for what db will need. Not complete, still needs lots of work

--- mongo_operations.js
+++ mongo_operations.js
@@ -2,6 +2,7 @@ var MongoClient = require("mongodb").MongoClient;
 var url = "mongodb://localhost:27017/";
 
 
+    //if anyone is reading this, ignore this function its for testing
     console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     MongoClient.connect(url, function(err, db) {
         console.log("bbbbbbbbbbbbbbbbbbbbbbbbbbb");
@@ -16,4 +17,64 @@ var url = "mongodb://localhost:27017/";
             db.close();
         });
     });
+
+
+    //from the creatPost file
+    function addPost(jsonPost) {
+        MongoClient.connect(url, function(err, db) {
+            db.collection("post").insertOne(jsonPost, function(err, res) {
+                if(err) {
+                    throw err;
+                }
+                db.close();
+            });
+        });
+    }
+
+    //when user clicks link on main page
+    function getFullPost(postID) {
+        var query = {ID: postID};
+        MongoClient.connect(url, function(err, db) {
+            db.collection("post").findOne(query, function(err, res) {
+                if(err) {
+                    throw err;
+                }
+                return res;
+                db.close();
+            });
+        });
+    }
+
+    //for getting the info for the main page using 25 items as a baseline
+    function mainPageList() {
+        
+        var display = {
+            _id: 0,
+            op: 1,
+            title: 1,
+            subject: 1,
+            replyCount: 1
+        };
+        MongoClient.connect(url, function(err, db) {
+            db.collection("post").find().toArray(function(err, postList) {
+                return postList;
+                db.close();
+            });
+        });
+    }
+
+    function increaseReplyCount() {
+
+    }
+    function getReplyCount() {
+
+    }
+    function listComment() {
+
+    }
+    function recursiveComment() {
+        
+    }
+
+
 
