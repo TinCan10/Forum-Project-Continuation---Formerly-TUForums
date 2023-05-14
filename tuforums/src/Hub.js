66527feb7f306849ed8b71c9e6fd381decbe1514
Joshua Nussbaumer <jnussb3@students.towson.edu>
2023-05-14 00:31:55 -0400

    renamed the component from app to hub

--- tuforums/src/Hub.js
+++ tuforums/src/Hub.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css';
 
-function App() {
+function Hub() {
     const [posts, setPosts] = React.useState([]);
   
     React.useEffect(() => {
@@ -42,4 +42,4 @@ function App() {
 }
   
 
-export default App;
+export default Hub;
--- tuforums/src/index.js
+++ tuforums/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Hub from './Hub';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Hub />
   </React.StrictMode>
 );
 
